a58e8e275fbdf65a1f8e5450cb3d6fdf922ed12b
Julia <julia0205ro@gmail.com>
2025-02-05 18:43:17 +0300

    Создан контейнер для REST API для проекта

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('PG_NAME'),
-        'HOST': os.environ.get('PG_HOST'),
-        'PORT': os.environ.get('PG_PORT'),
-        'USER': os.environ.get('PG_USER'),
-        'PASSWORD': os.environ.get('PG_PASSWORD'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
